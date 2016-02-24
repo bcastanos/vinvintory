c8b581ef2c48b917c0888c930c60f18e2d6583e6
Bryan Castanos <bcastanos@gmail.com>
2016-02-24 14:38:08 -0800

    login complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,20 +13,10 @@ class SessionsController < ApplicationController
       render 'new'
     end
 
-
-    # @user = User.find_by_email(params[:email])
-    # if @user && @user.authenticate(params[:password])
-    #   session[:user_id] = @user.id
-    #   flash[:success] = "Welcome! Thanks #{@user.email} for logging in!"
-    #   redirect_to user_path(@user)
-    # else
-    #   flash[:failure] = "Please try again."
-    #   redirect_to new_session_path
-    # end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 
